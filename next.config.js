fb9ccab9acba91eb44424826c789831dbd99342d
rintosaputro <saputrorinto@gmail.com>
2022-06-17 08:31:35 +0700

    feat: init project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_API_URL: process.env.NEXT_API_URL
+  }
 }
 
+// eslint-disable-next-line no-undef
 module.exports = nextConfig
